e86c831bcb92a97b8db2d20c9e308edbb0ac6d80
acasmith <acasmith101@gmail.com>
2018-12-07 22:25:14 +0000

    Created extremely simple file server

--- server.js
+++ server.js
@@ -1,20 +1,57 @@
-//Server for the game.
-//Experimenting with http module, I want to know what's underlying express a little rather than just hacking around.
+//Whilst exploring HTTP module, I've accidently started making a *really* borked HTTP server.
+//I want to know what's underlying express rather than just hacking around.
+
+//Process:
+//Check URL against files in parent directory.
+//If match, chunk and write it to socket. Else, return 404.
+//Currently doing everything synchronously to keep it simple, will refactor for asynch.
+
+//TODO:
+//Error handling in node, in this case so can throw 500 error back when read fails.
+//Handle images.
+//Refactor for asynch
+//Create and server 404 and 500 error pages.
+
+//Startup
 console.log("Server up!")
-const http = require('http')
-const server = http.createServer()
+const http = require('http');
+const fs = require('fs');
+
+//Create server, set listener and handler.
+const server = http.createServer();
 server.listen(8080, () => console.log("Listening on port 8080!"))
 server.on('request', (message, response) => {
-  fileContents = chunkFile("index.html")
-  response.write(fileContents);
+  console.log(message.url);
+  fileContents = chunkFile(message.url)
+  console.log(fileContents)
+  if(fileContents){
+    response.write(fileContents);
+    console.log(response.statusCode);
+  } else if(fileContents === -1){
+    response.statusCode = 500;
+    console.log(response.statusCode)
+  } else{
+    response.statusCode = 404;
+    console.log(response.statusCode);
+  }
   response.end();
 })
 
-const fs = require('fs')
+//Reads in a file synchronously, returns a string of the file contents.
 //Better to find a way to do a buffered read and write in chunks to response.
-//This helps prevent frontloading memory if the hosted files are large.
+//This helps prevent eating memory if the hosted files are large.
 function chunkFile(path){
-  const file = fs.readFileSync(path);
-  console.log(file);
-  return file;
+  //Catch main page request or set path relative to current directory.
+  if(path === '/'){
+    path = "index.html"
+  } else{
+    path = "." + path;
+  }
+
+  //Check file existence, and read it in if it exists.
+  //Try/catch doesn't work. Research how to error handle in node more.
+  if(fs.existsSync(path)){
+      const file = fs.readFileSync(path);
+      return file;
+  }
 }
